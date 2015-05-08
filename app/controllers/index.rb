1e17a0c45d26c4ae39ca5c74d37c1bb84820a47b
Taniya Prabhakar & Charlie Ward <pair+tani8.wardch@devbootcamp.com>
2015-05-08 15:53:21 -0700

    add working display all surveys and survey content

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,40 +1,80 @@
 get '/' do
 
-  redirect '/creator'
+  redirect '/'
 end
 
-get '/creator' do
-  @creator = Creator.all
+# get '/creator' do
+#   @creator = Creator.all
 
-  erb :index
-end
+#   erb :index
+# end
 
-#new
-get '/creator/new' do
-  @creator = Creator.new
+# #new
+# get '/creator/new' do
+#   @creator = Creator.new
 
-  erb :new_survey_form
-end
+#   erb :new_survey_form
+# end
 
-#create
-post '/creator' do
-  @creator = Creator.new(name: params[:name])
+# #create
+# post '/creator' do
+#   @creator = Creator.new(name: params[:name])
 
-end
+# end
 
+#homepage with creating or viewing options
 get '/home' do
   # @user = Creator.find(id: session[:id])
 
   erb :home
 end
 
-get "/home/form" do
+#create new form
+get "/home/survey" do
   erb :create_survey
 end
 
+post '/home/survey' do
+  @survey = Survey.create(
+    title: params[:title],
+    description: params[:description],
+    creator_id: session[:id]
+  )
+
+  @question = Question.create(
+    body: params[:body],
+    survey_id: @survey.id
+  )
+
+  @choice_string = params[:answer]
+
+  @choice_array = @choice_string.split(',')
+  @choice_array.each do |choice|
+    Choice.create(answer: choice, question_id: @question.id)
+
+  end
+   # erb :create_survey
+   # redirect '/home/display_survey'
+  erb :display_survey
+end
+
+get "/survey/:id" do
+  @survey = Survey.where(id: params[:id]).first
+  @question = @survey.questions
+
+  erb :display_survey
+end
+
+
+
+#displays all forms created
 get "/home/all" do
+  @surveys = Survey.all
+
   erb :view_all
 end
+
+
 
 
 
